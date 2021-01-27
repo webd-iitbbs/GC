a48eb533e2c6c2c6e856c99b3ffd84968ef7720c
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-01-27 18:53:25 +0530

    OK

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -6,7 +6,6 @@ router.use(bodyParser.json());
 
 router.get( "/leaderboard", async (req, res) => {
   const branches = await Branch.find().sort({ total: -1 });
-  res.sendStatus(200);
   res.json( branches );
 });
 
