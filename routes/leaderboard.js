a6ecfd6862338633a8d105a40bd6087ba1110f2e
S Sathiya Seelan <sss18@iitbbs.ac.in>
2021-02-02 08:44:35 +0530

    added absolute score to send to client

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const Branch = require("../models/Branch");
+const Society = require('../models/Society');
 const bodyParser = require('body-parser');
 const router = express.Router();
 router.use(bodyParser.json());
 
 router.get( "/leaderboard", async (req, res) => {
   const branches = await Branch.find().sort({ total: -1 });
-  res.json( branches );
+  const individuals = await Society.find();
+  res.json( branches, individuals );
 });
 
 module.exports = router;
